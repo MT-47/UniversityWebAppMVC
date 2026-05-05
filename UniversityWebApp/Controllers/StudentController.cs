37c1cb247af19d67807b4022d377010c5fb859f6
Mustafa Taher <mustafa.taher.abuelnasr@gmail.com>
2026-05-05 14:37:11 +0300

    feat: Add StudentController with ShowAll and Details

---
+++ UniversityWebApp/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UniversityWebApp.Models;
+
+namespace UniversityWebApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult ShowAll()
+        {
+            StudentBL studentBL = new StudentBL();
+            List<Student> StudentsListModel = studentBL.GetAllStudents();
+            return View("ShowAll", StudentsListModel);
+        }
+        public IActionResult Details(int id)
+        {
+            StudentBL studentBL = new StudentBL();
+            Student studentModel = studentBL.GetById(id);
+            return View("Details", studentModel);
+        }
+    }
+}
