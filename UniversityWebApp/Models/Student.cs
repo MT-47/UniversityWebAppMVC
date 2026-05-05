2899a4291a0774b65589e3ea9138a025e4f25da0
Mustafa Taher <mustafa.taher.abuelnasr@gmail.com>
2026-05-05 14:36:44 +0300

    feat: Add Student model and StudentBL

---
+++ UniversityWebApp/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UniversityWebApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string ImageUrl { get; set; }
+
+        public override string ToString() => $"Id : {Id} , Name : {Name} , Age : {Age} , Email : {Email} , ImageUrl : {ImageUrl}";
+
+    }
+}
---
+++ UniversityWebApp/Models/StudentBL.cs
@@ -0,0 +1,27 @@
+﻿namespace UniversityWebApp.Models
+{
+    public class StudentBL
+    {
+        List<Student> students;
+
+        public StudentBL()
+        {
+            students = new List<Student>();
+
+            students.Add(new Student { Id = 1, Name = "Alice", Age = 20, Email = "Alice@mail.com", ImageUrl = "alice.jpg" });
+            students.Add(new Student { Id = 2, Name = "Bob", Age = 22, Email = "Bob@mail.com", ImageUrl = "bob.jpg" });
+            students.Add(new Student { Id = 3, Name = "Ema", Age = 21, Email = "Ema@mail.com", ImageUrl = "ema.jpg" });
+            students.Add(new Student { Id = 4, Name = "David", Age = 23, Email = "David@mail.com", ImageUrl = "david.jpg" });
+
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            return students;
+        }
+        public Student GetById(int id)
+        {
+            return students.FirstOrDefault(s => s.Id == id);
+        }
+    }
+}
